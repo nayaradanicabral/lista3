df55c486a02311305347faa94f7686a372e6214c
nayaradanicabral <147215426+nayaradanicabral@users.noreply.github.com>
2023-10-08 17:30:08 -0300

    Update senhas.service.ts

--- src/app/services/senhas.service.ts
+++ src/app/services/senhas.service.ts
@@ -8,13 +8,13 @@ export class SenhasService {
   public senhasPrior: number = 0;
   public senhasExame: number = 0;
   public senhasTotal: number = 0;
-  public senhasArray: any = {
-    SG: [],
-    SP: [],
-    SE: []
-  };
+  
   public inputNovaSenha: string = '';
-
+  
+  public senhasArray: { [key: string]: string[] } = { SG: [], SP: [], SE: [] };
+  
+  public tempoMedioAtendimento: { [key: string]: number } = { SG: 1, SP: 0.67 };
+  // tempo 1 minuto e o outro de segundos 
   constructor() { }
 
   somaGeral(): void {
@@ -32,27 +32,41 @@ export class SenhasService {
     this.senhasTotal++;
   }
 
-  novaSenha(tipoSenha :string = '') {
+  novaSenha(tipoSenha : string = '') {
     let dataAtual = new Date();
-    let ano = dataAtual.getFullYear().toString().substring(2, 4);
+    let ano = dataAtual.getFullYear().toString().substring(2,4);
     let mes = dataAtual.getMonth().toString().padStart(2, '0');
     let dia = dataAtual.getDate().toString().padStart(2, '0');
-
+  
     if (tipoSenha == 'SG') {
       this.somaGeral();
-      this.inputNovaSenha = `${ano}${mes}${dia}-${tipoSenha}${(this.senhasArray['SG'].length + 1).toString().padStart(2, '1')}`;
-      this.senhasArray.SG.push(this.inputNovaSenha);
+      this.inputNovaSenha = ano + mes + dia + '-' + tipoSenha + (this.senhasArray['SG'].length+1).toString().padStart(2,'0');
+      this.senhasArray['SG'].push(this.inputNovaSenha); 
     } else if (tipoSenha == 'SP') {
       this.somaPrior();
-      this.inputNovaSenha = `${ano}${mes}${dia}-${tipoSenha}${(this.senhasArray['SP'].length + 1).toString().padStart(2, '1')}`;
-      this.senhasArray.SP.push(this.inputNovaSenha);
+      this.inputNovaSenha = ano + mes + dia + '-' + tipoSenha + (this.senhasArray['SP'].length+1).toString().padStart(2,'0');
+      this.senhasArray['SP'].push(this.inputNovaSenha); 
     } else if (tipoSenha == 'SE') {
       this.somaExame();
-      this.inputNovaSenha = `${ano}${mes}${dia}-${tipoSenha}${(this.senhasArray['SE'].length + 1).toString().padStart(2, '1')}`;
-      this.senhasArray.SE.push(this.inputNovaSenha);
+      this.inputNovaSenha = ano + mes + dia + '-' + tipoSenha + (this.senhasArray['SE'].length+1).toString().padStart(2,'0');
+      this.senhasArray['SE'].push(this.inputNovaSenha);
     }
     
     console.log(this.senhasArray);
   }
+
+  chamarProximoNaFila(): string {
+    if (this.senhasArray['SG'].length > 0) {
+      return this.senhasArray['SG'].shift();
+    } else if (this.senhasArray['SP'].length > 0) {
+      return this.senhasArray['SP'].shift();
+    }
+    return '';
+  }
+
+  calcularTempoMedioAtendimento(tipoSenha: string): number {
+    return this.senhasArray[tipoSenha].length * this.tempoMedioAtendimento[tipoSenha];
+  }
 }
+
 
